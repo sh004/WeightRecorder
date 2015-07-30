3d16712f09a0c496ced89eb62d80a3d59ede1756
ymmt <yamamoto@s14.yeskist.net>
2015-07-30 17:26:54 +0900

    入力　現在時間

--- WeightRecorder/app/src/main/java/com/weightrecorder/weightrecorder/InputActivity.java
+++ WeightRecorder/app/src/main/java/com/weightrecorder/weightrecorder/InputActivity.java
@@ -27,18 +27,7 @@ public class InputActivity extends ActionBarActivity  {
         weight = (EditText) findViewById(R.id.iWeight);
         buttonEnter = (Button) findViewById(R.id.eButton);
 
-        //現在の日時を表示
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);        // 0=1月 … 11=12月
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        //int week = cal.get(Calendar.DAY_OF_WEEK);
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-        int minute = cal.get(Calendar.MINUTE);
-
-        String date = year + "." + (month+1) + "." + day;   //+ "(" + week + ")";
-        String time = hour + ":" + minute;
-        dateAndTime.setText(date + " " + time);
+        dateAndTime.setText(getDateAndTime());
 
         //登録ボタン押下
         buttonEnter.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +48,26 @@ public class InputActivity extends ActionBarActivity  {
 
             }
         });
+    }
+
+    public static String getDateAndTime() {
+        String date, time;
+
+        //現在の日時を取得
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);                                  //西暦
+        int month = cal.get(Calendar.MONTH) + 1;                            //月
+        int day = cal.get(Calendar.DAY_OF_MONTH);                           //日
+        String[] week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};  //曜日
+        int weekNum = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        int hour = cal.get(Calendar.HOUR_OF_DAY);                           //時
+        int minute = cal.get(Calendar.MINUTE);                              //分
+
+        date = String.format("%1$04d.%2$02d.%3$02d", year, month, day) +  "(" + week[weekNum] + ")";
+        time =  String.format("%1$02d:%2$02d", hour, minute);
+        //time = hour + ":" + minute;
+
+        return date + " " + time;
     }
 
     @Override
